5c34ade45e7b32b8f6cfedb0ebd07be313d81bbb
David Morgan <DaveM2430@users.noreply.huggingface.co>
2026-04-30 23:54:52 +0000

    Update app.R

--- app.R
+++ app.R
@@ -1204,8 +1204,9 @@ pushViewport(viewport(x = 0.27, y = 0.82, width = 0.46, height = 0.23, just = c(
     suppressWarnings(print(p_right, newpage = FALSE))
     popViewport()
 
-    grid.text("Diamond = Hard Hit (95+ mph EV)", x = 0.5, y = 0.355,
-              gp = gpar(cex = 0.65, col = "grey40", fontface = "italic"))popViewport()
+grid.text("Diamond = Hard Hit (95+ mph EV)", x = 0.5, y = 0.355,
+              gp = gpar(cex = 0.65, col = "grey40", fontface = "italic"))
+    popViewport()
     }
 
     grid.text("Pitch Specifications", x = 0.5, y = 0.355,
