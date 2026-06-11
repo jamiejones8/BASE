2cce73eb51f49d36773ccdd5cec143841058f6f4
David Morgan <DaveM2430@users.noreply.huggingface.co>
2026-06-11 16:48:24 +0000

    Update leaderboards_embed.R

--- leaderboards_embed.R
+++ leaderboards_embed.R
@@ -19,7 +19,7 @@ whitecaps_env <- local({
 whitecaps_hub_card <- function() {
   list(
     id = "whitecaps_central",
-    title = "Whitecaps Analytics",
+    title = "Whitecaps Leaderboards",
     page = WHITECAPS_PAGE_ID,
     status = "live",
     image_src = whitecaps_hub_image_src
